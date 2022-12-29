2365d7189b7ac30ca42f16ade17fa3415d5734e7
khuong253 <113605715+khuong253@users.noreply.github.com>
2022-12-29 12:51:52 +0700

    update

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connect() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/football_manager"),
+    await mongoose.connect("mongodb://127.0.0.1:27017/football_manager"),
       {
         useNewUrlParser: true,
         useUnifedTopology: true,
